38e39842bc84362a4f11861c6cd790b1b11e3b0c
DESKTOP-N0I17L8\vegbr <veg.bro@hotmail.no>
2024-05-28 16:15:42 +0200

    Merge remote-tracking branch 'origin/frontend/vegardM-starting-UI' into frontend/setup
    # Conflicts:
    #	backend/app.py
    #	backend/util/create_processes_object.py

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <>
-      <img alt="w3Logo" src="../../public/img/v3logo.png" />
+      <img alt="w3Logo" src="../public/img/v3logo.png" />
     </>
   );
 };
