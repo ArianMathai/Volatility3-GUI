02d6575f1be81fcee9054f3633bc022f6d7d181c
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2024-05-30 14:20:49 +0200

    logo n more

---
+++ frontend2/app/src/components/shared/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <>
+            <img className="mb-10" alt="w3Logo" src="../public/img/v3logo.png" />
+        </>
+    );
+};
+
+export default Logo;
